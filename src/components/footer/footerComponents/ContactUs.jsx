6103c73b18f2d3cc56abda7894604024044d8613
Nihal Mohamed Bashir <nihalmb23@gmail.com>
2024-12-11 08:25:10 +0530

    minor changes

--- src/components/footer/footerComponents/ContactUs.jsx
+++ src/components/footer/footerComponents/ContactUs.jsx
@@ -2,7 +2,7 @@ import { FaPhoneAlt, FaEnvelope } from 'react-icons/fa'; // Importing icons
 
 const ContactUs = () => {
     return (
-        <div className=' border rounded-xl border-white md:px-32 flex flex-col text-center justify-center h-56 my-10'>
+        <div className=' border rounded-xl border-white md:px-20 flex flex-col text-center justify-center h-56 my-10'>
             <h1 className='text-[#0796EF] font-oswald text-2xl mb-4'>CONTACT WITH US</h1>
 
             <div className='flex items-center justify-center  text-white mb-2'>
